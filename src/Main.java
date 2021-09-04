a0b5aaa65eedddb41f49694edd68984a781f938a
Nurshakh <sarybeknurshah0@gmail.com>
2021-09-04 15:27:25 +0600

    Sorting algorithms

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 3, 5, 6, 2};
+        SortingAlgorithms.bubbleSort(array);
+        SortingAlgorithms.insertionSort(array);
+        SortingAlgorithms.selectionSort(array);
+    }
+}
