3366b0cb47a80247c87410ebc940b27273a3e698
Nurshakh <sarybeknurshah0@gmail.com>
2021-11-21 15:36:35 +0600

    swapping words

--- src/exercises/Test.java
+++ src/exercises/Test.java
@@ -1,6 +1,14 @@
 package exercises;
 
 public class Test {
+    public static void swapWords(String str, String str2) {
+        str += str2;
+        str2 = str.substring(0, (str.length()) - (str2.length()));
+        str = str.substring(str2.length());
+
+        System.out.println("str:" + " " + str + ", str2:" + " " + str2);
+    }
+
     public static void swap(int a, int b) {
 //        a += b;
 //        b = a - b;
@@ -14,5 +22,7 @@ public class Test {
         swap(5645, 341);
         swap(1, 3);
         swap(1000, 900);
+
+        swapWords("Mechanic", "Person");
     }
 }
