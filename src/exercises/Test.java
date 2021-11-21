f21c62de4e3acb86d412bfab6e1804b3a43109cb
Nurshakh <sarybeknurshah0@gmail.com>
2021-11-21 10:08:07 +0600

    swapping variables

--- src/exercises/Test.java
+++ src/exercises/Test.java
@@ -1,2 +1,18 @@
-package exercises;public class Test {
+package exercises;
+
+public class Test {
+    public static void swap(int a, int b) {
+//        a += b;
+//        b = a - b;
+//        a = a - b;
+        a = a + b - (b = a);
+
+        System.out.println("a:" + " " +  a + ", b:" + " " + b);
+    }
+
+    public static void main(String[] args) {
+        swap(5645, 341);
+        swap(1, 3);
+        swap(1000, 900);
+    }
 }
